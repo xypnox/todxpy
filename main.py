da63c348eb5b8022283778c10d2b664d73f253c0
xypnox <xypnox@gmail.com>
2018-10-26 11:47:06 +0530

    Added cmdline arguments in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from yesnoquery import query_yes_no as qyn
 import fabric
+import argparse
+import os
 
 def main():
     todol = fabric.TodoList(input("Enter title : "), input("Enter tags : ").split())
@@ -10,4 +12,17 @@ def main():
     todol.view_list()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A CLI ToDo App')
+    parser.add_argument('--add',
+                        help='add a todo')
+    parser.add_argument('--view',
+                        default='inbox',
+                        help='view the passed list by default views inbox')
+    parser.add_argument('--all',
+                        action='store_true',
+                        help='view all the files')
+    args = parser.parse_args()
+    print(args)
+    if args.add != None:
+        print("Added todo + ", args.add)
     main()
