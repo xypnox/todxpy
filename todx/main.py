cd4535b911447074051ddc62d23f50a96f8b1c20
Jay Rajput <jay24rajput@gmail.com>
2019-12-19 18:14:27 +0530

    Added help command functionality

--- todx/main.py
+++ todx/main.py
@@ -10,14 +10,14 @@ from . import parse_functions as pf
 from . import settings as stg
 
 def main_command():
+
     # Load datafile and parse arguments
     app_data_file = appdirs.user_data_dir('todx') + '/data.json'
 
     twrap = filehandler.load_file(app_data_file)
 
     args = sys.argv[1:]
-    # print('Arguments passed are : ', args)
-
+    #print('Arguments passed are : ', args)
     if len(args) == 0:
         args.append('task')
         pf.parse_task(twrap, args)
@@ -45,6 +45,11 @@ def main_command():
 
     elif args[0] == '--version' or args[0] == '-v':
         print('TodX v' + stg.version)
+
+    elif args[0] == '--help' or args[0] == '-h':
+        file_reader=open('todx/man.txt','r')
+        print(file_reader.read())
+
 
     elif args[0][0] == '+':
         args.insert(0, 'task')
